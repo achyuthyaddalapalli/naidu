60569f7508d00b23faad68a1cdd8b19a5ca1bd84
achyuthyaddalapalli <114048699+achyuthyaddalapalli@users.noreply.github.com>
2022-10-20 21:17:37 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,3 +1,7 @@
-def call(){
-   echo "tomcat deploy coming soon.........."
+def call(creds,tomcatIp,username,warName){
+   sshagent([creds]){
+      //copy war file to tomcat webapss floder
+      sh "scp -o stictHostkeychecking=no target/${warName}.war ${userName}@${tomcatIp}:/opt/tomcat9/webapps/"
+      sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh"
+      sh "ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh"
 }
